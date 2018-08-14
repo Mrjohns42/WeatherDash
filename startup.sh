1915ee356c3877d6bcada465852aafee272d4fa6
Matthew R. Johnson <mrjohns42@gmail.com>
2018-08-14 00:50:24 -0700

    updated startup script to work on raspbian

--- startup.sh
+++ startup.sh
@@ -1,20 +1,47 @@
 #!/bin/bash 
+
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 SERVER_DIR=${SCRIPT_DIR}/server
-SERVER_ADDR=http://localhost:8000
+SERVER_HOST=localhost
+SERVER_PORT=8000
+SERVER_ADDR=http://${SERVER_HOST}:${SERVER_PORT}
 
 CLIENT_DIR=${SCRIPT_DIR}/client
-CLIENT_ADDR=http://localhost:4200
+CLIENT_HOST=localhost
+CLIENT_PORT=4200
+CLIENT_ADDR=http://${CLIENT_HOST}:${CLIENT_PORT}
+
 
 cd ${SERVER_DIR}
+echo "Starting API Server in background.."
 x-terminal-emulator -e npm start
-ping ${SERVER_ADDR} -a
 
 cd ${CLIENT_DIR}
-x-terminal-emulator -e ng serve
-ping ${CLIENT_ADDR} -a
+echo "Building Client Application..."
+ng build --prod --aot --build-optimizer --output-hashing=none
+echo "Build Complete."
+
+cd ${CLIENT_DIR}/dist/WeatherClient
+echo "Starting Client Host in background..."
+x-terminal-emulator -e angular-http-server -p ${CLIENT_PORT}
+
+echo "Waitng for API Server to come online..."
+while ! nc -z ${SERVER_HOST} ${SERVER_PORT}
+do
+	sleep 1
+done
+echo "API Server online."
+
+echo "Waiting for Client Host to come online..."
+while ! nc -z ${CLIENT_HOST} ${CLIENT_PORT}
+do
+	sleep 1
+done
+echo "Client Host online."
 
 cd ${SCRIPT_DIR}
+echo "Launching WeatherDash"
 x-terminal-emulator -e chromium-browser --kiosk ${CLIENT_ADDR}
+
